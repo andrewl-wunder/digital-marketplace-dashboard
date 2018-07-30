b9c7ed2e9da46814f28c3de30dcb118f7cc183d4
Andrew Larcombe <andrew.larcombe@wunder.io>
2018-07-30 18:00:53 +0100

    localstorage stuff

--- parse-dos-data.js
+++ parse-dos-data.js
@@ -1,6 +1,38 @@
-function parse_dos_data(csv, org_or_supplier, segment = 'org') {
+function initialise_marketplace_data(callback) {
+
+  if (get_all_marketplace_data() == null) {
+    d3.csv('./opportunity-data.csv', function(err, data) {
+      console.log('writing csv data to local storage');
+      localStorage.setItem("dos-csv", JSON.stringify(data));
+      callback(null);
+    });
+  }
+  else {
+    callback(null);
+  }
+
+}
+
+
+function get_all_marketplace_data() {
+  data = localStorage.getItem("dos-csv");
+  if (data != null) {
+    return JSON.parse(data);
+  }
+  else {
+    return null;
+  }
+}
+
+//@todo - get synonyms
+
+function get_marketplace_data_for_org(buyer_or_supplier_name, segment = 'org') {
+
+  console.log('parsing dos data for ' + buyer_or_supplier_name);
+  csv = get_all_marketplace_data()
+
   var segment_data = {
-    'name': org_or_supplier,
+    'name': buyer_or_supplier_name,
     'summary': {},
     'opportunities':[],
     'opportunities_by_value':{},
@@ -8,19 +40,21 @@ function parse_dos_data(csv, org_or_supplier, segment = 'org') {
     'other_segment':{},
   }
 
+  buyer_or_supplier_names = ['Wunder Ltd','We Are Snook Ltd'];
+
   var filtered_csv;
   if (segment == 'org') {
-    console.log(org_or_supplier);
-    filtered_csv = csv.filter(function(opportunity) { return opportunity['Organisation Name'] === org_or_supplier; });
+    console.log(buyer_or_supplier_name);
+    filtered_csv = csv.filter(function(opportunity) { return opportunity['Organisation Name'] === buyer_or_supplier_name; });
   }
   else {
-    filtered_csv = csv.filter(function(opportunity) { return opportunity['Winning supplier'] === org_or_supplier; });
+    filtered_csv = csv.filter(function(opportunity) { return buyer_or_supplier_names.includes(opportunity['Winning supplier']); });
   }
 
   segment_data.opportunities = filtered_csv;
 
   var o = {};
-  o.name = org_or_supplier;
+  o.name = buyer_or_supplier_name;
   o.children = [];
   filtered_csv.forEach(function(opportunity) {
     if (isNaN(parseFloat(opportunity['Contract amount']))) {
@@ -44,10 +78,10 @@ function parse_dos_data(csv, org_or_supplier, segment = 'org') {
   }
 
   var supplier_values = {};
-  supplier_values.name = org_or_supplier;
+  supplier_values.name = buyer_or_supplier_name;
   supplier_values.children = [];
   var supplier_totals = {};
-  supplier_totals.name = org_or_supplier;
+  supplier_totals.name = buyer_or_supplier_name;
   supplier_totals.children = [];
   other_segment_query_param = segment == 'supplier' ? 'o' : 's';
   suppliers.forEach(function(supplier) {
